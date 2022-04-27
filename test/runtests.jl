c78084b0c653a8f69dc487488640c55bc15c1c65
Nathan Foulk <natefoulk4@gmail.com>
2022-04-27 18:07:30 -0400

    Added tests for runge rectangle integration

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using Test, ChebInt
 
-@testset "Trivial tests" begin
+@testset "ChebNodes" begin
     @test ChebNodes1D(3) == [-0.866025403784439, 0.0, 0.866025403784439]
     @test ChebNodes1D(3, -2, 7) == [-1.397114317029974, 2.5, 6.397114317029974]
+end
+@testset "rectangle integration" begin
+    @test rectangles(runge, 1000, -1, 1) ≈ 0.5493603314328083
 end
