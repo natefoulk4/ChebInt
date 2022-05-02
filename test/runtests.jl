f55687277806e65ffa369dbb8189802949a05412
Nathan Foulk <natefoulk4@gmail.com>
2022-05-02 17:16:15 -0400

    Added test for chebInt

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,11 +8,14 @@ end
     @test centeredmesh(10, 0, 1)[1] == 0.05
     @test centeredmesh(10, 0, 1)[end] == 0.95
 end
-@testset "integration" begin
+@testset "rectangles" begin
     res = recInt(runge, 1000, -1, 1) 
     @test res[1] ≈ 0.5493603314328083
     @test isapprox(res[2],  runge(0.0) ; rtol=1e3)
     res2 = recInt(runge, 1000, -1, 1; nelectrons=1) 
     @test isapprox(res2[1], 0.07324432682131453; rtol=1e5)
     @test isapprox(res2[2],  runge(0.5) ; rtol=1e3)
+end
+@testset "ChebInt" begin
+    chebInt(runge, 1000, -1, 1) ≈ 0.54936030677803
 end
